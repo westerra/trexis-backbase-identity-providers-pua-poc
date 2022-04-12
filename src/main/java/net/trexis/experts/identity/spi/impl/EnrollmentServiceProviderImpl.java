3417b0ee8d7fd5afd08bf70a27f6788530114836
Hendrik Tredoux <hendrik.tredoux@trexis.net>
2022-04-12 15:23:03 -0400

    Implement support to call enrollment for limited and rebase procedures.

---
+++ src/main/java/net/trexis/experts/identity/spi/impl/EnrollmentServiceProviderImpl.java
@@ -0,0 +1,33 @@
+package net.trexis.experts.identity.spi.impl;
+
+import net.trexis.experts.identity.spi.EnrollmentServiceProvider;
+import org.jboss.logging.Logger;
+import org.keycloak.models.UserModel;
+
+public class EnrollmentServiceProviderImpl implements EnrollmentServiceProvider {
+    private static final Logger log = Logger.getLogger(EnrollmentServiceProviderFactoryImpl.class);
+    private EnrollmentServiceClient client;
+
+    public EnrollmentServiceProviderImpl() {
+        log.info("Creating Enrollment Service Provider");
+        EnrollmentServiceProperties properties = EnrollmentServiceProviderFactoryImpl.getEnrollmentServiceProperties();
+        client = new EnrollmentServiceClient(properties);
+    }
+
+    @Override
+    public void callRebaseService(UserModel userModel) {
+        log.info("Calling enrollment service, user rebase: " + client.toString());
+        client.startRebase(userModel.getUsername());
+    }
+
+    @Override
+    public boolean callEvaluateLimitedService(UserModel userModel) {
+        log.info("Calling enrollment service, user validate limited: " + client.toString());
+        return client.evaluateLimited(userModel.getUsername());
+    }
+
+    @Override
+    public void close() {
+        // do nothing
+    }
+}
